b6d7dfdbe42f78a2b675d26dd46e40f83764e8db
eAKmcdowell <maxime.lorentz2@etu.unistra.fr>
2014-11-14 14:35:58 +0100

    debut recherche points verts

--- src/img/Image.java
+++ src/img/Image.java
@@ -1,9 +1,11 @@
 package img;
 
 import java.awt.Color;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
@@ -38,7 +40,51 @@ public class Image {
 	
 	
 	public static void main(String[] arg){
-		Image img = new Image("img\\book_left.jpg");
+		Image Myimg = new Image("img\\pv.jpg");
+		//Image Myimg = new Image("img\\empty_left.jpg");
+		
+		int width = Myimg.img.getWidth();
+		int height = Myimg.img.getHeight();
+		
+		float hue;
+		boolean in;
+		
+		ArrayList<Point> li = new ArrayList<Point>();
+		boolean point_pres = false;
+		int r  = 4;
+		for(int i = r; i < width - r; i++){
+			for(int j = r; j < height - r; j++){
+				
+				in = true;
+				for(int m = -r; m < r + 1 && in; m++){
+					for(int n = -r; n < r + 1 && in; n++){
+						hue = Myimg.hue[i+m][j+n];
+						if(hue < 0.23 || hue > 0.43)
+							in = false;
+					}
+				}
+				if(in){
+					Point p1 = new Point(i,j);
+					li.add(p1);		
+				}	
+			}
+		}
+		
+		System.out.println(li);
+		
+		ArrayList<Point> centres = new ArrayList<Point>();
+		
+		for(Point pi : li){
+			boolean isCentre = true;
+			for(Point pj : centres){
+				if(Math.abs(pj.x - pi.x) < 15 && Math.abs(pj.y - pi.y) < 15)
+					isCentre = false;
+			}
+			if(isCentre)
+				centres.add(pi);
+		}
+		System.out.println(centres);
+		
 		
 		
 	}
